ea629487c9612ea3d259e1335d7a8eb718fd67c7
EDWARD <edward.kang0301@gmail.com>
2024-09-10 20:57:56 +1000

    Create starting base PDF for each Excel File

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from pathlib import Path
 import pandas as pd
-import fpdf
+from fpdf import FPDF
 import glob
 
 filepaths = glob.glob("invoices/*")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
 
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
