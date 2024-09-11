4ffb403ae65d308b2332ca47b6b424de54129f5d
EDWARD <edward.kang0301@gmail.com>
2024-09-11 17:53:52 +1000

    Added Date to pdf

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ import glob
 filepaths = glob.glob("invoices/*")
 
 for filepath in filepaths:
+    # Import data file
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
+    # Extract names
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
 
+    # Create title and date
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date {date}")
+
+
     pdf.output(f"PDFs/{filename}.pdf")
